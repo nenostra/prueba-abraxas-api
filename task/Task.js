fb6646400683519b47cd5c5bd8497754727d101f
nenostra <neno.24@hotmail.com>
2018-05-27 12:20:59 -0500

    Changed model

--- task/Task.js
+++ task/Task.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
   title: String,
   description: String,
-  duration: String,
+  duration: Number,
   started: Boolean,
   completed: Boolean,
+  createdAt: Date,
 });
 
 const Task = mongoose.model('Task', TaskSchema);
--- task/taskRouter.js
+++ task/taskRouter.js
@@ -14,6 +14,7 @@ router.post('/', asyncMiddleware(async (req, res, next) => {
     ...req.body,
     started: false,
     completed: false,
+    createdAt: new Date(),
   });
   res.json(createdTask);
 }));
